556538d6d0cc0b64caa7916506fc6b04426b7bf2
Victor-Alexandru <victor21cuciureanu@gmail.com>
2019-09-04 23:18:29 +0300

    added the solution

--- AssignCookies.py
+++ AssignCookies.py
@@ -1,23 +1,20 @@
 class Solution:
-    def findContentChildren(self, g, s) -> int:
-        g = sorted(g)
-        s = sorted(s)
-        total_cookies = 0
-        greed_max = max(g)
-        size_max = max(s)
-        if greed_max < size_max:
-            return min(len(g), len(s))
-        size_index = 0
-        for greed in g:
-               if size_index > len(s):
-                      break
-               if greed <= s[size_index]:
-                      size_index += 1
-                      total_cookies += 1
-               else:
-                      return total_cookies
-        return total_cookies
-
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        if not g or not s:
+            return 0
+        g.sort()
+        s.sort()
+        res = 0
+        child_i = 0 
+        for cake in s:
+            if child_i >= len(g):
+                return res
+            if cake >= g[child_i]:
+                res += 1
+                child_i += 1
+        
+        return res
 
 s = Solution()
-print(s.findContentChildren([1,2,3], [1,1]))
+print(s.findContentChildren(
+    [10, 9, 8, 7], [5, 6, 7, 8]))
